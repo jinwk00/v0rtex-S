734972bd07f48336b81ec9214324405c93ba17ba
Ben Sparkes <bensparkes8@gmail.com>
2017-12-22 06:42:58 +0000

    [symbols] added a few offsets

--- v0rtex-S/symbols.m
+++ v0rtex-S/symbols.m
@@ -87,6 +87,51 @@ BOOL init_symbols()
         }
     }
     
+    else if (strcmp(u.machine, "iPhone8,1") == 0)
+    {
+        if ([ver isEqual: @"Version 10.3.3 (Build 14G60)"])
+        {
+            OFFSET_ZONE_MAP                            = 0xfffffff007548478;
+            OFFSET_KERNEL_MAP                          = 0xfffffff0075a4050;
+            OFFSET_KERNEL_TASK                         = 0xfffffff0075a4048;
+            OFFSET_REALHOST                            = 0xfffffff00752aba0;
+            OFFSET_BZERO                               = 0xfffffff007081f80;
+            OFFSET_BCOPY                               = 0xfffffff007081dc0;
+            OFFSET_COPYIN                              = 0xfffffff0071803a0;
+            OFFSET_COPYOUT                             = 0xfffffff007180594;
+            OFFSET_CHGPROCCNT                          = 0xfffffff007049e01;
+            OFFSET_KAUTH_CRED_REF                      = 0xfffffff007367c18;
+            OFFSET_IPC_PORT_ALLOC_SPECIAL              = 0xfffffff007099e94;
+            OFFSET_IPC_KOBJECT_SET                     = 0xfffffff0070ad16c;
+            OFFSET_IPC_PORT_MAKE_SEND                  = 0xfffffff0070999b8;
+            OFFSET_IOSURFACEROOTUSERCLIENT_VTAB        = 0xfffffff006e7c9f8;
+            OFFSET_ROP_ADD_X0_X0_0x10                  = 0xfffffff006462174;
+            OFFSET_ROP_LDR_X0_X0_0x10                  = 0xfffffff0073690d8;
+            OFFSET_ROOT_MOUNT_V_NODE                   = 0xfffffff0075a40b0;
+        }
+    }
+    
+    else if (strcmp(u.machine, "iPhone6,1") == 0)
+    {
+        if ([ver isEqual: @"Version 10.3.3 (Build 14G60)"])
+        {
+            OFFSET_ZONE_MAP                             = 0xfffffff00754c478;
+            OFFSET_KERNEL_MAP                           = 0xfffffff0075a8050;
+            OFFSET_KERNEL_TASK                          = 0xfffffff0075a8048;
+            OFFSET_REALHOST                             = 0xfffffff00752eba0;
+            OFFSET_BZERO                                = 0xfffffff007081f80;
+            OFFSET_BCOPY                                = 0xfffffff007081dc0;
+            OFFSET_COPYIN                               = 0xfffffff007180e98;
+            OFFSET_COPYOUT                              = 0xfffffff00718108c;
+            OFFSET_IPC_PORT_ALLOC_SPECIAL               = 0xfffffff007099f14;
+            OFFSET_IPC_KOBJECT_SET                      = 0xfffffff0070ad1ec;
+            OFFSET_IPC_PORT_MAKE_SEND                   = 0xfffffff007099a38;
+            OFFSET_IOSURFACEROOTUSERCLIENT_VTAB         = 0xfffffff006f25538;
+            OFFSET_ROP_ADD_X0_X0_0x10                   = 0xfffffff006522174;
+            OFFSET_ROOT_MOUNT_V_NODE                    = 0xfffffff0075a80b0;
+        }
+    }
+    
     else
     {
         LOG("Device not supported.");
